6975adecce1c1a81b1ee9cf89c3afe7fbb70904f
Numan Sadık Şen <68291382+NumaN2004@users.noreply.github.com>
2021-06-22 18:53:32 +0300

    Delete Waves directory

--- Waves/dummy.lua
+++
@@ -1,8 +0,0 @@
-tim = 0
-
-function Update()
-	tim = tim+1
-	if tim == 30 then
-		EndWave()
-	end
-end
--- Waves/nothing.lua
+++
@@ -1,8 +0,0 @@
-spawntime=0
-require "Libraries/Playermove"
-function Update()
-spawntime=spawntime+1
-Arena.ResizeImmediate(565,130)
---Encounter.SetVar("Arena",Arena)
-EndWave()
-end
